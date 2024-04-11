807d15aeff236d476cf058a757609ed0b7aec13a
SangWon7242 <gotkdqja@gmail.com>
2024-04-11 21:11:29 +0900

    C:/Program Files/Git/usr/article/write 기능 구현

--- src/main/java/com/sbs/exam/board/Main.java
+++ src/main/java/com/sbs/exam/board/Main.java
@@ -12,14 +12,25 @@ public class Main {
       System.out.printf("명령) ");
       String cmd = sc.nextLine();
 
-      if(cmd.equals("exit")) {
+      if(cmd.equals("/usr/article/write")) {
+        System.out.println("== 게시물 작성 ==");
+
+        System.out.printf("제목) ");
+        String title = sc.nextLine();
+
+        System.out.printf("내용) ");
+        String body = sc.nextLine();
+
+        int id = 1;
+
+        System.out.printf("%d번 게시물이 생성되었습니다.\n", id);
+      }
+      else if(cmd.equals("exit")) {
         System.out.println("== 게시판을 종료합니다 ==");
         break;
       }
-
-      System.out.printf("입력받은 명령어 : %s\n", cmd);
-
-      sc.close();
     }
+
+    sc.close();
   }
 }
