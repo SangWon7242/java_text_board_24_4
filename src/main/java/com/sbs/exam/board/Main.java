6c54f40d9699721c93c2d3dc31b7db377f3c869b
SangWon7242 <gotkdqja@gmail.com>
2024-04-16 20:11:47 +0900

    게시물 상세보기 기능 구현

--- src/main/java/com/sbs/exam/board/Main.java
+++ src/main/java/com/sbs/exam/board/Main.java
@@ -6,6 +6,7 @@ public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int articleLastId = 0;
+    Article lastArticle = null;
 
     System.out.println("== 텍스트 게시판 v 0.1 ==");
     System.out.println("프로그램 시작");
@@ -26,10 +27,26 @@ public class Main {
         int id = ++articleLastId;
 
         Article article = new Article(id, title, body);
+        lastArticle = article;
 
         System.out.println("생성된 게시물 객체 : " + article);
 
         System.out.printf("%d번 게시물이 생성되었습니다.\n", article.id);
+      }
+      else if(cmd.equals("/usr/article/detail")) {
+        if (lastArticle == null) {
+          System.out.println("게시물이 존재하지 않습니다.");
+          continue;
+        }
+
+        Article article = lastArticle;
+
+        System.out.println("== 게시물 상세보기 ==");
+        System.out.printf("번호 : %d\n", article.id);
+        System.out.printf("제목 : %s\n", article.title);
+        System.out.printf("내용 : %s\n", article.body);
+
+
       }
       else if(cmd.equals("exit")) {
         System.out.println("== 게시판을 종료합니다 ==");
