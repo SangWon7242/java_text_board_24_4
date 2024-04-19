92d0f8c4028e1286aa37d60b1aaa6b8335c88673
SangWon7242 <gotkdqja@gmail.com>
2024-04-19 21:17:51 +0900

    orderBy=idDesc 파라미터에 의한 정렬방법 변경

--- src/main/java/com/sbs/exam/board/Main.java
+++ src/main/java/com/sbs/exam/board/Main.java
@@ -51,10 +51,24 @@ public class Main {
         System.out.println("== 게시물 리스트 ==");
         System.out.println("번호 | 제목");
 
-        for(int i = articles.size() - 1; i >= 0; i--) {
-          Article article = articles.get(i);
-          System.out.printf(" %d  | %s\n", article.id, article.title);
+        boolean orderByIdDesc = true; // 기존로직
+
+        if(params.containsKey("orderBy") && params.get("orderBy").equals("idAsc")) {
+          orderByIdDesc = false;
         }
+
+        if(orderByIdDesc) {
+          for(int i = articles.size() - 1; i >= 0; i--) {
+            Article article = articles.get(i);
+            System.out.printf(" %d  | %s\n", article.id, article.title);
+          }
+        }
+        else {
+          for(Article article : articles) {
+            System.out.printf(" %d  | %s\n", article.id, article.title);
+          }
+        }
+
 
       } else if (rq.getUrlPath().equals("/usr/article/detail")) {
 
