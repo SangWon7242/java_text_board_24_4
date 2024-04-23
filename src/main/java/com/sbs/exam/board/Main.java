54c6cf5cebe41af6b73129f0fe0d60127f8ee98e
SangWon7242 <gotkdqja@gmail.com>
2024-04-23 21:26:08 +0900

    클래스를 별도의 파일로 분리

--- src/main/java/com/sbs/exam/board/Main.java
+++ src/main/java/com/sbs/exam/board/Main.java
@@ -18,7 +18,7 @@ public class Main {
 
     makeTestData();
 
-    if(articles.size() > 0) {
+    if (articles.size() > 0) {
       articleLastId = articles.get(articles.size() - 1).id;
     }
 
@@ -53,7 +53,7 @@ public class Main {
   private static void actionUsrArticleDelete(Rq rq) {
     Map<String, String> params = rq.getParams();
 
-    if(params.containsKey("id") == false) {
+    if (params.containsKey("id") == false) {
       System.out.println("id를 입력해주세요.");
       return;
     }
@@ -74,7 +74,7 @@ public class Main {
 
     Article article = findById(id, articles);
 
-    if(article == null) {
+    if (article == null) {
       System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
       return;
     }
@@ -87,7 +87,7 @@ public class Main {
   private static void actionUsrArticleModify(Scanner sc, Rq rq) {
     Map<String, String> params = rq.getParams();
 
-    if(params.containsKey("id") == false) {
+    if (params.containsKey("id") == false) {
       System.out.println("id를 입력해주세요.");
       return;
     }
@@ -108,7 +108,7 @@ public class Main {
 
     Article article = findById(id, articles);
 
-    if(article == null) {
+    if (article == null) {
       System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
       return;
     }
@@ -142,7 +142,7 @@ public class Main {
   private static void actionUsrArticleDetail(Rq rq) {
     Map<String, String> params = rq.getParams();
 
-    if(params.containsKey("id") == false) {
+    if (params.containsKey("id") == false) {
       System.out.println("id를 입력해주세요.");
       return;
     }
@@ -163,7 +163,7 @@ public class Main {
 
     Article article = findById(id, articles);
 
-    if(article == null) {
+    if (article == null) {
       System.out.printf("%d번 게시물은 존재하지 않습니다.", id);
       return;
     }
@@ -175,23 +175,21 @@ public class Main {
 
   }
 
-
-
   private static void actionUsrArticleList(Rq rq) {
     Map<String, String> params = rq.getParams();
 
     // 검색 시작
     List<Article> filteredArticles = articles;
 
-    if(params.containsKey("searchKeyword")) {
+    if (params.containsKey("searchKeyword")) {
       String searchKeyword = params.get("searchKeyword");
 
       filteredArticles = new ArrayList<>();
 
-      for(Article article : articles) {
+      for (Article article : articles) {
         boolean matched = article.title.contains(searchKeyword) || article.body.contains(searchKeyword);
 
-        if(matched) {
+        if (matched) {
           filteredArticles.add(article);
         }
       }
@@ -202,11 +200,11 @@ public class Main {
 
     boolean orderByIdDesc = true; // 기존로직
 
-    if(params.containsKey("orderBy") && params.get("orderBy").equals("idAsc")) {
+    if (params.containsKey("orderBy") && params.get("orderBy").equals("idAsc")) {
       orderByIdDesc = false;
     }
 
-    if(orderByIdDesc) {
+    if (orderByIdDesc) {
       sortedArticles = Util.reverseList(sortedArticles);
     }
 
@@ -214,14 +212,14 @@ public class Main {
     System.out.println("== 게시물 리스트 ==");
     System.out.println("번호 | 제목");
 
-    for(Article article : sortedArticles) {
+    for (Article article : sortedArticles) {
       System.out.printf(" %d  | %s\n", article.id, article.title);
     }
   }
 
   private static Article findById(int id, List<Article> articles) {
-    for(Article article : articles) {
-      if(article.id == id) {
+    for (Article article : articles) {
+      if (article.id == id) {
         return article;
       }
     }
@@ -230,78 +228,6 @@ public class Main {
   }
 }
 
-class Article {
-  int id;
-  String title;
-  String body;
 
-  Article(int id, String title, String body) {
-    this.id = id;
-    this.title = title;
-    this.body = body;
-  }
 
-  @Override // 어노테이션
-  public String toString() {
-    return String.format("{id : %d, title : \"%s\", body : \"%s\"}", id, title, body);
-  }
-}
 
-class Rq {
-  String url;
-  Map<String, String> params;
-  String urlPath;
-
-  Rq(String url) {
-    this.url = url;
-    params = Util.getParamsFromUrl(this.url);
-    urlPath = Util.getUrlPathFromUrl(this.url);
-  }
-
-  public Map<String, String> getParams() {
-    return params;
-  }
-
-  public String getUrlPath() {
-    return urlPath;
-  }
-}
-
-class Util {
-  static Map<String, String> getParamsFromUrl(String url) {
-    Map<String, String> params = new HashMap<>();
-    String[] urlBits = url.split("\\?", 2);
-
-    if(urlBits.length == 1) {
-      return params;
-    }
-
-    String queryString = urlBits[1];
-
-    for(String bit : queryString.split("&")) {
-      String[] bits = bit.split("=", 2);
-
-      if(bits.length == 1) {
-        continue;
-      }
-
-      params.put(bits[0], bits[1]); // key, value
-    }
-
-    return params;
-  }
-
-  static String getUrlPathFromUrl(String url) {
-    return url.split("\\?", 2)[0];
-  }
-
-  // 이 함수는 원본리스트를 훼손하지 않고, 새 리스트를 만듭니다. 즉 정렬이 반대인 복사본리스트를 만들어서 반환합니다.
-  public static<T> List<T> reverseList(List<T> list) {
-    List<T> reverse = new ArrayList<>(list.size());
-
-    for ( int i = list.size() - 1; i >= 0; i-- ) {
-      reverse.add(list.get(i));
-    }
-    return reverse;
-  }
-}
