3cceffee7562e877306e610b5ae2a0dcdb51d844
SangWon7242 <gotkdqja@gmail.com>
2024-04-30 20:14:52 +0900

    롬복 적용하여 소스코드 다이어트

--- src/main/java/com/sbs/exam/board/Article.java
+++ src/main/java/com/sbs/exam/board/Article.java
@@ -1,42 +1,12 @@
 package com.sbs.exam.board;
 
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Article {
   private int id;
   private String title;
   private String body;
-
-  public void setId(int id) {
-    this.id = id;
-  }
-
-  public void setTitle(String title) {
-    this.title = title;
-  }
-
-  public void setBody(String body) {
-    this.body = body;
-  }
-
-  public int getId() {
-    return id;
-  }
-
-  public String getTitle() {
-    return title;
-  }
-
-  public String getBody() {
-    return body;
-  }
-
-  public Article(int id, String title, String body) {
-    this.id = id;
-    this.title = title;
-    this.body = body;
-  }
-
-  @Override // 어노테이션
-  public String toString() {
-    return String.format("{id : %d, title : \"%s\", body : \"%s\"}", id, title, body);
-  }
 }
--- src/main/java/com/sbs/exam/board/container/Container.java
+++ src/main/java/com/sbs/exam/board/container/Container.java
@@ -3,12 +3,16 @@ package com.sbs.exam.board.container;
 import com.sbs.exam.board.controller.ArticleController;
 import com.sbs.exam.board.controller.MemberController;
 import com.sbs.exam.board.session.Session;
+import lombok.Getter;
 
 import java.util.Scanner;
 
 public class Container {
   public static Scanner sc;
+
+  @Getter
   public static Session session;
+
   public static ArticleController articleController;
   public static MemberController memberController;
 
@@ -18,9 +22,5 @@ public class Container {
 
     articleController = new ArticleController();
     memberController = new MemberController();
-  }
-
-  public static Session getSession() {
-    return session;
   }
 }
